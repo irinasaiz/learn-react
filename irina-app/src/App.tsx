6ec84e4401bf77340d10dbd3a4cdecc05987ee4b
Irina-Ioana Saiz <irina.saiz@bolt.eu>
2026-01-31 09:20:12 +0200

    Basic types in ts

--- irina-app/src/App.tsx
+++ irina-app/src/App.tsx
@@ -5,12 +5,18 @@ import React from 'react'
 
 class App extends React.Component {
  render() {
+
+  //const firstValue: string = "Irina";
+  //const firstValue: number = 23;
+  //const firstValue: number[] = [2,3,4];
+  const firstValue: Array<number> = [2,3,4];
+
   return (
     <div className="App">
       <header className="App-header">
         <img src ={irina} className ="App-logo" alt="logo" />
         <p>
-          The Value
+          The Value {firstValue} is of {typeof firstValue} type!
         </p>
     </header>
   </div>
